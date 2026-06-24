5291983893adbb2076755ae9135c5e6ab6a0edba
seemitx <oatphuchit04@gmail.com>
2026-06-24 23:21:44 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,7 +10,6 @@
  *   const { auth, db } = window.SharePayConfig;
  */
 
-import { initializeApp }  from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth }        from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore }   from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getStorage }     from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
