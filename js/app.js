fce4b9df249d5cf867ad45118192d74dbc473c88
seemitx <oatphuchit04@gmail.com>
2026-06-24 21:20:44 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -17,13 +17,13 @@ import { getStorage }     from "https://www.gstatic.com/firebasejs/10.7.1/fireba
 
 // ===== Firebase Config =====
 const firebaseConfig = {
-  apiKey:            "YOUR_API_KEY",
-  authDomain:        "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId:         "YOUR_PROJECT_ID",
-  storageBucket:     "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId:             "YOUR_APP_ID",
-  measurementId:     "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyDgf7rYlJriOnE8_ieyCkY52sNWE2Upg_I",
+  authDomain: "sharepay-36d88.firebaseapp.com",
+  projectId: "sharepay-36d88",
+  storageBucket: "sharepay-36d88.firebasestorage.app",
+  messagingSenderId: "29935540555",
+  appId: "1:29935540555:web:199714c22d5217621df0ce",
+  measurementId: "G-Y47T7B2CRV"
 };
 
 // ===== Firestore Collection Names =====
