136517d55264850b1ec9e5debc5086f53ef2a7d6
seemitx <oatphuchit04@gmail.com>
2026-06-25 01:59:21 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -17,8 +17,6 @@ import { getStorage }     from "https://www.gstatic.com/firebasejs/10.7.1/fireba
 
 // ===== Firebase Config =====
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
